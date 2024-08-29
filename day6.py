8be9577b38730cbd355101b3de11cfd46d4c3a2e
HajeraSultana <e031306@gmail.com>
2024-08-29 07:37:37 +0100

    Create day6.py

---
+++ day6.py
@@ -0,0 +1,14 @@
+print("MY LOGIN SYSTEM")
+print("+++++++++++++++")
+username = input("User Name> ")
+password = input("Password> ")
+if username == "Sam" and password == "Sam123":
+  print("""Hello sam you're doing great job, keep it up!
+Raise and shine! May today bring you joy, success, and fulfillment.""")
+elif username == "Mohammed" and password == "Mohammed456":
+  print ("""Morning Mohammed, Wishing you a bright and beautiful day.keep up you're goign to be a great programmer!""")
+elif username == "Haj" and password == "Haj000":
+  print ("""Good morning Haj!
+wishing you a fresh start and wonderful day ahead.""")
+else:
+  print("Invalid login credentails")
